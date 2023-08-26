993ba5de8d28bcf820a89c5e94c916cd548273f1
Antonije <antonijek@yahoo.com>
2023-08-26 07:59:29 +0200

    added transaltions

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -3,27 +3,24 @@ import classes from "./sidebar.module.scss";
 import { Layout, Menu } from "antd";
 import { useTranslation } from "react-i18next";
 import { TeamOutlined, CarOutlined, ScheduleOutlined } from "@ant-design/icons";
+import { Link, useLocation } from "react-router-dom";
 
 const { Sider } = Layout;
-
-function getItem(label, key, icon, children) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  };
-}
-
-const items = [
-  getItem("Clients", "sub1", <TeamOutlined />),
-  getItem("Vehicles", "2", <CarOutlined />),
-  getItem("Reservations", "1", <ScheduleOutlined />),
-];
 
 const Sidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
   const { t } = useTranslation();
+  const location = useLocation();
+
+  const items = [
+    { key: "sub1", icon: <TeamOutlined />, label: "clients" },
+    { key: "sub2", icon: <CarOutlined />, label: "vehicles" },
+    { key: "sub3", icon: <ScheduleOutlined />, label: "reservations" },
+  ];
+
+  const activeKey = items.find((item) =>
+    location.pathname.includes(item.label.toLowerCase())
+  )?.key;
 
   return (
     <Sider
@@ -33,12 +30,13 @@ const Sidebar = () => {
       className={classes["sidebar"]}
     >
       <div className="demo-logo-vertical" />
-      <Menu
-        theme="dark"
-        defaultSelectedKeys={["1"]}
-        mode="inline"
-        items={items}
-      />
+      <Menu theme="dark" defaultSelectedKeys={[activeKey]} mode="inline">
+        {items.map((item) => (
+          <Menu.Item key={item.key} icon={item.icon}>
+            <Link to={`/${item.label.toLowerCase()}`}>{t(item.label)}</Link>
+          </Menu.Item>
+        ))}
+      </Menu>
     </Sider>
   );
 };
